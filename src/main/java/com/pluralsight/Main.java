277ed5be6b05ed9435bf4b0f33b6e2e9fc6393d4
dajamiller <dajamiller96@gmail.com>
2025-09-22 14:28:56 -0700

    programming is cool my name is daja

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Programming is so cool!");
-        System.out.println('My name is Daja Miller!')
+        System.out.println("My name is Daja Miller!");
     }
 }
